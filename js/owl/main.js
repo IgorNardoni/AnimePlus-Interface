532fde4f5be96d77470575fd2b90879a50278cab
Nardoni <igornardonik@gmail.com>
2022-05-04 16:02:11 -0300

    update

---
+++ js/owl/main.js
@@ -0,0 +1,11 @@
+window.alert('Welcome to AnimePlus - The greatest repository of anime and manga in Brazil / Bem vindo ao AnimePlus');
+
+function Reminders() {
+    console.log('Lembrar de mais adiante, trocar o idioma de portugês para inglês, de acordo com o window.alert')
+    console.log('Lembrar de adicionar mais interação com o usuário, como informações da série ao passar o mouse, estilo PrimeVideo')
+    console.log('Lembrar de adicionar as outras páginas de conteúdo')
+    console.log('Lembrar de construir o back-end')
+    console.log('Lembrar de mais adiante, temtar refatorar os códigos com mais JavaScript Core ou com ReactJS e Bootstrap')
+}
+
+    Reminders()
